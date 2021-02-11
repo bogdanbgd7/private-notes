951809d8c0dd35cc3b01ae53f1fcaf1e1a76a8a4
bogdanbgd7 <bogdan.ponocko@gmail.com>
2021-02-11 17:37:54 +0100

    NoteCell init

---
+++ private-notes/View/NoteCell.swift
@@ -0,0 +1,19 @@
+//
+//  NoteCell.swift
+//  private-notes
+//
+//  Created by Bogdan Ponocko on 11.2.21..
+//
+
+import UIKit
+
+class NoteCell: UITableViewCell {
+    
+    //MARK: - Outlets
+    @IBOutlet var noteLabel: UILabel!
+    @IBOutlet var lockImage: UIImageView!
+    
+
+    
+    
+}
