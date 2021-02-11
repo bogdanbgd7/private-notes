51bf5547994072da3710bb80e8835a3c6f48eeb2
bogdanbgd7 <bogdan.ponocko@gmail.com>
2021-02-11 11:45:04 +0100

    created note model

---
+++ private-notes/Enums/LockStatus.swift
@@ -0,0 +1,13 @@
+//
+//  LockStatus.swift
+//  private-notes
+//
+//  Created by Bogdan Ponocko on 11.2.21..
+//
+
+import Foundation
+
+enum LockStatus {
+    case isLocked
+    case notLocked
+}
---
+++ private-notes/Model/Note.swift
@@ -0,0 +1,20 @@
+//
+//  Note.swift
+//  private-notes
+//
+//  Created by Bogdan Ponocko on 11.2.21..
+//
+
+import Foundation
+
+class Note {
+    
+    //This property can be read, but cannot be set from the outside
+    public private(set) var message = "hi"
+    public var lockedStatus : LockStatus
+    
+    init(message : String, lockedStatus: LockStatus) {
+        self.message = message
+        self.lockedStatus = lockedStatus
+    }
+}
